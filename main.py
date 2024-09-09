e2b13674c470022ab42f38210432029f2fb69e4d
clara-verret <clara.verret@student-cs.fr>
2024-09-09 15:10:03 +0200

    create main and change monte carlo

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from monte_carlo_direct import below_chord_length_spheres, k_estimate_shperes
-from display import plot_k
+import argparse
 
-def __main__() :
-    #plot_k()
+from monte_carlo_direct import main_monte_carlo
+
+
+def main() :
+    """Compute either the direct or inverse problem"""
+    parser = argparse.ArgumentParser(description="parser used to chose which problem to solve")
+    # Create a mutually exclusive group
+    group = parser.add_mutually_exclusive_group()
+
+    #direct problem
+    group.add_argument('-d', '--direct', action='store_true', help="solve direct problem")
+    #inverse problem
+    group.add_argument('-i', '--inverse', action='store_true', help="solve inverse problem")
+
+    args = parser.parse_args()
+    if args.direct:
+        print("Solving direct problem")
+        main_monte_carlo() # dans le futur mettre la fonction qui construit CLD
+    elif args.inverse:
+        print("Solving inverse problem")
+        #mettre la fonction qui inverse le problème
+
+
+if __name__ == '__main__':
+    main()
--- monte_carlo_direct.py
+++ monte_carlo_direct.py
@@ -1,19 +1,39 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+
+N=100000
 
 #### SPHERES
 
-def below_chord_length_spheres(l,r): #l is the length of the chord, r is the radius of the circle
+###Global variables
+R=1
+
+def below_chord_length_spheres(array_l,r): #l is the length of the chord, r is the radius of the circle
     """suppose we have a circle of radius r, and we draw* a chord of length l, what is the probability that the chord is below l
     we can use monte carlo to estimate this probability"""
     d = random.uniform(0, r)
     L=2 * np.sqrt(r**2 - d**2)
-    return L<l
+    return L<array_l
 
-def k_estimate_shperes(l,r,n): # n is the number of random points
+def k_estimate_spheres(array_l,r,n): # n is the number of random points
     """monte carlo estimate of k the probability that a chord of length variable L is below l knowing the circle of radius r"""
-    count=0
+    count=np.zeros(len(array_l))
     for _ in range(n):
-        if below_chord_length_spheres(l,r):
-            count+=1
+        is_successful = below_chord_length_spheres(array_l,r)
+        for index, value in enumerate(is_successful):
+            if value :
+                count[index]+=1
     return count/n
+
+def main_monte_carlo():
+    """main function to compute the monte carlo estimate of k"""
+    list_l=np.linspace(0,2*R,100)
+    k_estimation=k_estimate_spheres(list_l,R,N)
+    #plot the result
+    plt.plot(list_l,k_estimation,'ro')
+    plt.xlabel("l")
+    plt.ylabel("k for r=1")
+    plt.savefig("monte_carlo_estimate_k_sphere.png")
+
+    return list_l,k_estimation

4a7d349f9533365b5abee3d88f3172afe1526ddc
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2024-09-12 16:27:15 +0200

    clean 4 shapes polyhedrons, start of report

--- mesh.py
+++ mesh.py
@@ -44,11 +44,12 @@ class polyhedron:
         y = random.uniform(y_min, y_max)
         return y
     
-    def random_chord_length(self) :
+    def random_chord_length(self, y = None): 
         """
         Returns the length of a random chord of the polyhedron
         """
-        y = self.random_y()
+        if y is None:
+            y = self.random_y()
         
         over_y = []
         under_y = []
@@ -69,19 +70,24 @@ class polyhedron:
 
         return max(intersections_x) - min(intersections_x)
     
-    def plot_proj(self, name) :
+    def plot_proj(self, name, chord_y = None) :
         """
         plots the projected points in MeshPlots/projections/name
 
         Args :
         name : name of the file
+        chord_y : y coordinate of the chord to plot. If None, no chord is plotted
         """
         plt.figure()
         plt.plot(*zip(*self.projected_vertices), 'o')
-        for i in range(4):
-            for j in range(i+1,4):
+        n = len(self.projected_vertices)
+        for i in range(n):
+            for j in range(i+1,n):
                 plt.plot(*zip(self.projected_vertices[i],self.projected_vertices[j]), color='b')
+        if chord_y is not None :
+            plt.axhline(y=chord_y, color='r')
         plt.savefig("MeshPlots/projections/"+name)
+        plt.close()
 
     def plot_3d(self, name) :
         """
@@ -92,11 +98,13 @@ class polyhedron:
         """
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        for i in range(4):
-            for j in range(i+1,4):
+        n = len(self.vertices)
+        for i in range(n):
+            for j in range(i+1,n):
                 ax.plot(*zip(self.vertices[i],self.vertices[j]), color='b')
         ax.scatter(*zip(*self.vertices))
         plt.savefig("MeshPlots/3D/"+name)
+        plt.close()
     
 class regular_tetrahedron(polyhedron) :
     """
@@ -111,7 +119,7 @@ class regular_tetrahedron(polyhedron) :
         self.H = r*np.sqrt(6)/3 #height of the tetrahedron
         self.b = r/(2*np.sqrt(3)) #distance from the center to the center of a face
         self.a = r/np.sqrt(3) #distance from the center to a vertex
-        self.vertices = np.array([(-1/2,-self.b,0), (1/2,-self.b,0), (0,self.a,0), (0,0,self.H)])
+        self.vertices = np.array([(-self.r/2,-self.b,0), (self.r/2,-self.b,0), (0,self.a,0), (0,0,self.H)])
         self.projected_vertices = np.array(self.vertices[:,:2])
 
     def check_edges_length(self):
@@ -131,8 +139,37 @@ class cube(polyhedron) :
     """
     def __init__(self, r = 1):
         self.r = r
-        self.vertices = np.array([(x,y,z) for x in [-1/2,1/2] for y in [-1/2,1/2] for z in [-1/2,1/2]])
+        self.vertices = r*np.array([(x,y,z) for x in [-1/2,1/2] for y in [-1/2,1/2] for z in [-1/2,1/2]])
         self.projected_vertices = np.array([(x,y) for x,y,z in self.vertices])
+
+class regular_pyramid(polyhedron) :
+    """
+    An element of this class represents a regular pyramid (base = square, summit above the center of the square, all edges of the same length), using the polyhedron class.
+
+    Args :
+    r : length of the edges of the pyramid
+    """
+    def __init__(self, r = 1):
+        self.r = r
+        self.h = r*np.sqrt(2)/2 #height of the pyramid
+        self.normalized_h = np.sqrt(2)/2 #height of the pyramid, with r = 1
+        self.vertices = r*np.array([(-1/2,-1/2,0), (1/2,-1/2,0), (1/2,1/2,0), (-1/2,1/2,0), (0,0,self.normalized_h)])
+        self.projected_vertices = np.array(self.vertices[:,:2])
+
+class regular_triangular_prism(polyhedron) :
+    """
+    An element of this class represents a regular triangular prism (equilateral triangle base, all edges of the same length) (basically, a tent), using the polyhedron class.
+
+    Args :
+    r : length of the edges of the prism
+    """
+    def __init__(self, r = 1):
+        self.r = r
+        self.h = r*np.sqrt(3)/2 #height of the prism
+        self.normalized_h = np.sqrt(3)/2 #height of the prism, with r = 1
+        self.vertices = r*np.array([(-1/2,-1/2,0), (1/2,-1/2,0), (1/2,1/2,0), (-1/2,1/2,0), (-1/2,0,self.normalized_h), (1/2,0,self.normalized_h)])
+        self.projected_vertices = np.array(self.vertices[:,:2])
+
 
 def random_uniform_rotation_matrix() :
     return Rotation.random().as_matrix()
@@ -148,6 +185,7 @@ def plot_cumulative_cld_hist(chords, name, bins=100) :
     plt.figure()
     plt.hist(chords, bins=bins, cumulative=True, density=True, histtype='step')
     plt.savefig("MeshPlots/clds/"+name)
+    plt.close()
 
 def plot_cld_hist(chords, name, bins=100) :
     """
@@ -159,6 +197,7 @@ def plot_cld_hist(chords, name, bins=100) :
     plt.figure()
     plt.hist(chords, bins=bins, density=True)
     plt.savefig("MeshPlots/clds/"+name)
+    plt.close()
 
 def get_regular_tetrahedron_list_of_chords(r = 1, N = 10000) :
     """
@@ -192,15 +231,63 @@ def get_cube_list_of_chords(r = 1, N = 10000) :
         list_of_chords.append(cube_.random_chord_length())
     return list_of_chords
 
-def get_tetrahedron_cld() :
-    chords = get_regular_tetrahedron_list_of_chords(N = 524288)
-    plot_cld_hist(chords, 'cld_tetra', bins=4096)
-    plot_cumulative_cld_hist(chords,'cumulative_cld_tetra', bins=4096)
+def get_regular_pyramid_list_of_chords(r = 1, N = 10000) :
+    """
+    Returns the list of N chord lengths of a regular pyramid with edges of length r
 
-def get_cube_cld() :
-    chords = get_cube_list_of_chords(N = 524288)
-    plot_cld_hist(chords, 'cld_cube', bins=4096)
-    plot_cumulative_cld_hist(chords,'cumulative_cld_cube', bins=4096)
+    Args :
+    r : length of the edges of the pyramid
+    N : number of chords
+    """
+    pyramid = regular_pyramid(r)
+    list_of_chords = []
+    for i in range(N) :
+        Gamma = random_uniform_rotation_matrix()
+        pyramid.rotate(Gamma)
+        list_of_chords.append(pyramid.random_chord_length())
+    return list_of_chords
+
+def get_regular_triangular_prism_list_of_chords(r = 1, N = 10000) :
+    """
+    Returns the list of N chord lengths of a regular triangular prism with edges of length r
+
+    Args :
+    r : length of the edges of the prism
+    N : number of chords
+    """
+    prism = regular_triangular_prism(r)
+    list_of_chords = []
+    for i in range(N) :
+        Gamma = random_uniform_rotation_matrix()
+        prism.rotate(Gamma)
+        list_of_chords.append(prism.random_chord_length())
+    return list_of_chords
+
+def get_tetrahedron_cld(N = 524288, bins=4096) :
+    chords = get_regular_tetrahedron_list_of_chords(N = N)
+    plot_cld_hist(chords, 'cld_tetra', bins=bins)
+    plot_cumulative_cld_hist(chords,'cumulative_cld_tetra', bins=bins)
+
+def get_cube_cld(N = 524288, bins=4096) :
+    chords = get_cube_list_of_chords(N = N)
+    plot_cld_hist(chords, 'cld_cube', bins=bins)
+    plot_cumulative_cld_hist(chords,'cumulative_cld_cube', bins=bins)
+
+def get_pyramid_cld(N = 524288, bins=4096) :
+    chords = get_regular_pyramid_list_of_chords(N = N)
+    plot_cld_hist(chords, 'cld_pyramid', bins=bins)
+    plot_cumulative_cld_hist(chords,'cumulative_cld_pyramid', bins=bins)
+
+def get_prism_cld(N = 524288, bins=4096) :
+    chords = get_regular_triangular_prism_list_of_chords(N = N)
+    plot_cld_hist(chords, 'cld_prism', bins=bins)
+    plot_cumulative_cld_hist(chords,'cumulative_cld_prism', bins=bins)
+
+def get_all_cld(N = 524288, bins=4096) :
+    get_tetrahedron_cld(N = N, bins=bins)
+    get_cube_cld(N = N, bins=bins)
+    get_pyramid_cld(N = N, bins=bins)
+    get_prism_cld(N = N, bins=bins)
 
 def plot_sample_tetrahedrons() :
     zone1 = []
@@ -209,18 +296,48 @@ def plot_sample_tetrahedrons() :
         tetrahedron = regular_tetrahedron()
         Gamma = random_uniform_rotation_matrix()
         tetrahedron.rotate(Gamma)
-        chord_length = tetrahedron.random_chord_length()
+        y = tetrahedron.random_y()
+        chord_length = tetrahedron.random_chord_length(y)
         if chord_length < 0.5 :
-            zone1.append(tetrahedron)
+            zone1.append((tetrahedron, y))
         if chord_length > 0.8 :
-            zone2.append(tetrahedron)
+            zone2.append((tetrahedron, y))
     
-    for i,tetra in enumerate(zone1) :
-        tetra.plot_proj('tetra_zone1_'+str(i))
+    for i,(tetra,y) in enumerate(zone1) :
+        tetra.plot_proj('tetra_zone1_'+str(i), y)
         tetra.plot_3d('tetra_zone1_'+str(i))
 
-    for i,tetra in enumerate(zone2) :
-        tetra.plot_proj('tetra_zone2_'+str(i))
+    for i,(tetra,y) in enumerate(zone2) :
+        tetra.plot_proj('tetra_zone2_'+str(i), y)
         tetra.plot_3d('tetra_zone2_'+str(i))
 
-plot_sample_tetrahedrons()
+def plot_sample_cubes() :
+    zone1 = []
+    zone2 = []
+    zone3 = []
+    for i in range(300) :
+        cube_ = cube()
+        Gamma = random_uniform_rotation_matrix()
+        cube_.rotate(Gamma)
+        y = cube_.random_y()
+        chord_length = cube_.random_chord_length(y)
+        if chord_length < 0.8 :
+            zone1.append((cube_, y))
+        if (chord_length > 1.1) and (chord_length < 1.3) :
+            zone2.append((cube_, y))
+        if chord_length > 1.5 :
+            zone3.append((cube_, y))
+    
+    for i,(cube_,y) in enumerate(zone1) :
+        cube_.plot_proj('cube_zone1_'+str(i), y)
+        cube_.plot_3d('cube_zone1_'+str(i))
+
+    for i,(cube_,y) in enumerate(zone2) :
+        cube_.plot_proj('cube_zone2_'+str(i), y)
+        cube_.plot_3d('cube_zone2_'+str(i))
+
+    for i,(cube_,y) in enumerate(zone3) :
+        cube_.plot_proj('cube_zone3_'+str(i), y)
+        cube_.plot_3d('cube_zone3_'+str(i))
+
+get_all_cld(N = 1048576, bins=8192)
